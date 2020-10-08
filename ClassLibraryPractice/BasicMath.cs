c6346ade95f5fdf902bfceecdc1b6f39dea5c909
Tejas Naik <tejasrajeevnaik@live.com>
2020-10-08 14:56:43 -0700

    Adding two projects - ClassLibraryPractice and ClassLibraryTest

---
+++ ClassLibraryPractice/BasicMath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryPractice
+{
+    public class BasicMath
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ ClassLibraryTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryPractice;
+
+namespace ClassLibraryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void VerifyAddition()
+        {
+            int result = BasicMath.Addition(10, 15);
+            Assert.AreEqual(25, result);
+        }
+    }
+}
