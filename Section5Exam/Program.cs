16e82b35c0417f3846823ba3bb96f5002a733255
Tejas Naik <tejasrajeevnaik@live.com>
2020-10-11 17:13:21 -0700

    October-11-2020 work

---
+++ Section5Exam/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section5Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string yearString;
+            int yearInt;
+            
+            while (true)
+            {
+                Console.Write("Enter the year: ");
+                yearString = Console.ReadLine();
+
+                if(int.TryParse(yearString, out yearInt))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid year. Please try again.");
+                }
+            }
+
+            DisplayLeapYearOrNot(yearInt);
+        }
+
+        private static void DisplayLeapYearOrNot(int year)
+        {
+            bool isLeapYear = false;
+            bool divisibleBy4 = false;
+            bool divisibleBy100 = false;
+            bool divisibleBy400 = false;
+
+            if (0 == year % 4)
+            {
+                divisibleBy4 = true;
+                isLeapYear = true;
+
+                if (0 == year % 100)
+                {
+                    divisibleBy100 = true;
+                    isLeapYear = false;
+                }
+                if (0 == year % 400)
+                {
+                    divisibleBy400 = true;
+                    isLeapYear = true;
+                }
+            }
+
+            Console.WriteLine($"The entered year: {year}");
+            Console.WriteLine($"Divisible by 4: {divisibleBy4}");
+            Console.WriteLine($"Divisible by 100: {divisibleBy100}");
+            Console.WriteLine($"Divisible by 400: {divisibleBy400}");
+            Console.WriteLine($"Leap year: {isLeapYear}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest2.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    /// <summary>
+    /// Summary description for UnitTest2
+    /// </summary>
+    [TestClass]
+    [TestCategory("Section 5 Quiz")]
+    public class UnitTest2
+    {
+        public UnitTest2()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void Convert_F_To_C()
+        {
+            double tempInF = 100;
+            double tempInC = (tempInF - 32) * ((double)5 / (double)9);
+            Assert.AreEqual(37.778, tempInC, 0.001);
+        }
+
+        [TestMethod]
+        public void Convert_C_To_F()
+        {
+            float tempInC = 45;
+            float tempInF = tempInC * ((float)9 / (float)5) + 32;
+            Assert.AreEqual(113, tempInF);
+        }
+    }
+}
